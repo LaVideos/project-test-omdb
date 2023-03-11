ff41306bdd634c07db97fcc231b1ac755096ed4a
LaVideos <lavideos1313@gmail.com>
2023-03-11 21:52:11 +0200

    last

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
-import {ServerStyleSheets} from '@material-ui/core/styles';
+import {ServerStyleSheets} from "@material-ui/styles";
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
